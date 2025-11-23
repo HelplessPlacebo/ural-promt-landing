41f984a9b3c6478b99bcd2fab963b9bae803873b
Vladislav Stepanenko <vladislavstepanenko@Vladislavs-MacBook-Air.local>
2025-11-23 19:23:27 +0300

    feat: migrate to static build, decompose api to another service

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+    output: "export",
 
   turbopack: {
     root: __dirname,
--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -44,7 +44,7 @@ export function Contact() {
         setStep('loading');
 
         try {
-            const res = await fetch('/api/send-email', {
+            const res = await fetch('https://api.ural-promt.ru/send-email', {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify(formData),
