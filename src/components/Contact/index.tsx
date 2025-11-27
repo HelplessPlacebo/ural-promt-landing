b5ff586d70a84dd184f33752a7c0c9bd929289e1
Vladislav Stepanenko <vladislavstepanenko@Vladislavs-MacBook-Air.local>
2025-11-27 16:30:04 +0300

    chore: updated send email endpoint

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -44,7 +44,7 @@ export function Contact() {
     setStep('loading');
 
     try {
-      const res = await fetch('https://api.ural-promt.ru/send-email', {
+      const res = await fetch('https://api.ural-promt.ru', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
